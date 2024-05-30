7bc27f7a76c306fad585690523f4c79365173684
Kye Davey <kye.j.davey@gmail.com>
2024-05-30 21:04:03 +1000

    add new kata

---
+++ javascript/8kyu/even-or-odd.js
@@ -0,0 +1,5 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+// My Solution
+
+const evenOrOdd = (n) => n % 2 === 0 ? "Even" : "Odd";
