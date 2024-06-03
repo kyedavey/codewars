b571eae10466f5cf3cc89a0a6faf7d33b8980e30
Kye Davey <kye.j.davey@gmail.com>
2024-06-03 18:20:33 +1000

    add new kata

---
+++ basic-mathematical-operations.js
@@ -0,0 +1,25 @@
+// Your task is to create a function that does four basic mathematical operations.
+
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+
+// My Solution
+function basicOp(operation, value1, value2){
+  return eval(`${value1} ${operation} ${value2}`);
+}
+
+// Using Switch statement
+function basicOp(operation, value1, value2) {
+  switch (operation) {
+      case '+':
+          return value1 + value2;
+      case '-':
+          return value1 - value2;
+      case '*':
+          return value1 * value2;
+      case '/':
+          return value1 / value2;
+      default:
+          return 0;
+  }
+}
