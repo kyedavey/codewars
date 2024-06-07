ede3a2c0fdff3f7bc9cded1d3d102a9ef1455bb7
Kye Davey <kye.j.davey@gmail.com>
2024-06-07 10:58:37 +1000

    add new kata

---
+++ javascript/8kyu/you-only-need-one-beginner.js
@@ -0,0 +1,16 @@
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+// Array can contain numbers or strings. X can be either.
+// Return true if the array contains the value, false if not.
+
+// My Solution
+function check(a, x) {
+  return a.includes(x);
+}
+
+// Arrow Function
+const check = (a, x) => a.includes(x);
+
+// Using indexOf
+function check(a, x) {
+  return a.indexOf(x) > -1;
+}
