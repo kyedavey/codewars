9c80a4ca88aaa4fe308b78e20ca368b84dfb44fb
Kye Davey <kye.j.davey@gmail.com>
2024-06-11 19:25:36 +1000

    add new kata

---
+++ javascript/grasshopper-personalized-message.js
@@ -0,0 +1,6 @@
+// Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+// My Solution
+function greet(name, owner) {
+  return name === owner ? "Hello boss" : "Hello guest";
+}
