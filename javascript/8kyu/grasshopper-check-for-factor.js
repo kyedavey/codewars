3389e02be222c45f2be862cc41eaa75d25b4ad09
Kye Davey <kye.j.davey@gmail.com>
2024-06-13 13:07:07 +1000

    add new kata

---
+++ javascript/8kyu/grasshopper-check-for-factor.js
@@ -0,0 +1,7 @@
+// This function should test if the factor is a factor of base.
+// Return true if it is a factor or false if it is not.
+
+// My Solution
+function checkForFactor(base, factor) {
+  return !(base % factor);
+}
