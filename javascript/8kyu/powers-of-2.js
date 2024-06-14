c50c016fe4cd0dec9d668f622f7c0fa720a789bc
Kye Davey <kye.j.davey@gmail.com>
2024-06-14 17:05:02 +1000

    add new kata

---
+++ javascript/8kyu/powers-of-2.js
@@ -0,0 +1,10 @@
+// Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+
+// My Solution
+function powersOfTwo(n) {
+  let result = [];
+  for (let i = 0; i <= n; i++) {
+    result.push(2 ** i);
+  }
+  return result;
+}
