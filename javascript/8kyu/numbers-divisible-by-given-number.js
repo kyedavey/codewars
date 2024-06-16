d407ee701af070fba857da79521e2ae23bf83fd3
Kye Davey <kye.j.davey@gmail.com>
2024-06-16 11:35:40 +1000

    add new kata

---
+++ javascript/8kyu/numbers-divisible-by-given-number.js
@@ -0,0 +1,6 @@
+// Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. First argument is an array of numbers and the second is the divisor.
+
+// My Solution
+function divisibleBy(numbers, divisor) {
+  return numbers.filter((n) => n % divisor === 0);
+}
