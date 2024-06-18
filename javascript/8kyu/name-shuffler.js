b2f56a17022c1aad6dc483c8eb4294abc60acc46
Kye Davey <kye.j.davey@gmail.com>
2024-06-19 09:50:00 +1000

    add new kata

---
+++ javascript/8kyu/name-shuffler.js
@@ -0,0 +1,6 @@
+// Write a function that returns a string in which firstname is swapped with last name.
+
+// My Solution
+function nameShuffler(str) {
+  return str.split(" ").reverse().join(" ");
+}
