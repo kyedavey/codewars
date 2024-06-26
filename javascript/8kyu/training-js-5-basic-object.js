b8b2fae37b03c9a8e51c77a375ac7f8f2ad14372
Kye Davey <kye.j.davey@gmail.com>
2024-06-26 12:35:50 +1000

    add new kata

---
+++ javascript/8kyu/training-js-5-basic-object.js
@@ -0,0 +1,6 @@
+// In javascript, Object is one of basic data types. Define an Object can use var obj=new Object() or var obj={}
+
+// My Solution
+function animal(obj) {
+  return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
+}
