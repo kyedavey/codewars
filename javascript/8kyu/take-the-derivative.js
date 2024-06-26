2a8a6584371c95beb563258ccdb4058935504d5c
Kye Davey <kye.j.davey@gmail.com>
2024-06-26 13:24:18 +1000

    add new kata

---
+++ javascript/8kyu/take-the-derivative.js
@@ -0,0 +1,7 @@
+// This function takes two numbers as parameters, the first number being the coefficient, and the second number being the exponent.
+// Your function should multiply the two numbers, and then subtract 1 from the exponent. Then, it has to return an expression (like 28x^7). "^1" should not be truncated when exponent = 2.
+
+// My Solution
+function derive(coefficient, exponent) {
+  return `${coefficient * exponent}x^${exponent - 1}`;
+}
