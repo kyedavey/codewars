466fc1a6104ab4a3d208efd0aa46ba5624a2cbae
Kye Davey <kye.j.davey@gmail.com>
2024-07-02 13:04:19 +1000

    add new kata

---
+++ javascript/8kyu/check-same-case.js
@@ -0,0 +1,18 @@
+// Write a function that will check if two given characters are the same case.
+// If either of the characters is not a letter, return -1
+// If both characters are the same case, return 1
+// If both characters are letters, but not the same case, return 0
+
+// My Solution
+function sameCase(a, b) {
+  if (!a.match(/^[A-Za-z]$/) || !b.match(/^[A-Za-z]$/)) {
+    return -1;
+  } else if (
+    (a.match(/^[A-Z]$/) && b.match(/^[A-Z]$/)) ||
+    (a.match(/^[a-z]$/) && b.match(/^[a-z]$/))
+  ) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
