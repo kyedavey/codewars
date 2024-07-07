f880c54e4751e093dd43d866c06c9638d6b7fb3a
Kye Davey <kye.j.davey@gmail.com>
2024-07-07 11:02:25 +1000

    add new kata

---
+++ javascript/8kyu/oop-object-oriented-piracy.js
@@ -0,0 +1,14 @@
+// You have access to the ship "draft" and "crew". "Draft" is the total ship weight and "crew" is the number of humans on the ship.
+// Each crew member adds 1.5 units to the ship draft. If after removing the weight of the crew, the draft is still more than 20, then the ship is worth looting. Any ship weighing that much must have a lot of booty!
+
+// My Solution
+class Ship {
+  constructor(draft, crew) {
+    this.draft = draft;
+    this.crew = crew;
+  }
+
+  isWorthIt() {
+    return this.draft - this.crew * 1.5 > 20;
+  }
+}
