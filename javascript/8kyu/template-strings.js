6bcc2eb32ccfacab012f2658cf8a5a9f16f9a446
Kye Davey <kye.j.davey@gmail.com>
2024-07-08 12:10:48 +1000

    add new kata

---
+++ javascript/8kyu/template-strings.js
@@ -0,0 +1,6 @@
+// Your task is to return the correct string using the Template String Feature
+
+// My Solution
+var templateStrings = function (noun, adjective) {
+  return `${noun} are ${adjective}`;
+};
