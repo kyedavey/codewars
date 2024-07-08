c5c7612570459a37ebad197054c6a979202e45c3
Kye Davey <kye.j.davey@gmail.com>
2024-07-08 11:17:31 +1000

    add new kata

---
+++ javascript/8kyu/duck-duck-goose.js
@@ -0,0 +1,6 @@
+// Task: Given an array of Player objects (an array of associative arrays in PHP) and an index (1-based), return the name of the chosen Player(name is a property of Player objects, e.g Player.name)
+
+// My Solution
+function duckDuckGoose(players, goose) {
+  return players[(goose - 1) % players.length].name;
+}
