7f0c51bb4af5bb7c81b21f55049f7e7505e3bc67
Kye Davey <kye.j.davey@gmail.com>
2024-07-18 18:07:57 +1000

    add new kata

---
+++ javascript/8kyu/easy-logs.js
@@ -0,0 +1,6 @@
+// Given a logarithm base X and two values A and B, return a sum of logarithms with the base X.
+
+// My Solution
+function logs(x, a, b) {
+  return Math.log(a * b) / Math.log(x);
+}
