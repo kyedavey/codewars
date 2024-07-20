a8dec21676c9ba1e338077eccf29b41709cc0cd6
Kye Davey <kye.j.davey@gmail.com>
2024-07-21 09:30:29 +1000

    add new kata

---
+++ javascript/8kyu/be-concise-i-the-ternary-operator.js
@@ -0,0 +1,19 @@
+// You are given a function describeAge / describe_age that takes a parameter age (which will always be a positive integer) and does the following:
+// If the age is 12 or lower, it return "You're a(n) kid"
+// If the age is anything between 13 and 17 (inclusive), it return "You're a(n) teenager"
+// If the age is anything between 18 and 64 (inclusive), it return "You're a(n) adult"
+// If the age is 65 or above, it return "You're a(n) elderly"
+// Your task is to shorten the code as much as possible. Note that submitting the given code will not work because there is a character limit of 137.
+
+// My Solution
+function describeAge(age) {
+  return `You're a(n) ${
+    age <= 12
+      ? "kid"
+      : age >= 13 && age <= 17
+      ? "teenager"
+      : age >= 18 && age <= 64
+      ? "adult"
+      : "elderly"
+  }`;
+}
