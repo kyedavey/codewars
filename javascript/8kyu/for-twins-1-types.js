fa34b69130f6b085e49049d9fbfa4fc1fbf00717
Kye Davey <kye.j.davey@gmail.com>
2024-07-21 16:21:21 +1000

    add new kata

---
+++ javascript/8kyu/for-twins-1-types.js
@@ -0,0 +1,6 @@
+// Write a function that will accept two parameters: variable and type and check if type of variable is matching type. Return true if types match or false if not.
+
+// My Solution
+function typeValidation(variable, type) {
+  return typeof variable === type;
+}
