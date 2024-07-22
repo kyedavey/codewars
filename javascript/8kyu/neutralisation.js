1aed9daaa82bfbe310f842ffb2cc9053c5011c09
Kye Davey <kye.j.davey@gmail.com>
2024-07-22 12:51:05 +1000

    add new kata

---
+++ javascript/8kyu/neutralisation.js
@@ -0,0 +1,13 @@
+// Given two strings comprised of + and -, return a new string which shows how the two strings interact in the following way:
+// When positives and positives interact, they remain positive.
+// When negatives and negatives interact, they remain negative.
+// But when negatives and positives interact, they become neutral, and are shown as the number 0.
+
+// My Solution
+function neutralise(s1, s2) {
+  let result = "";
+  for (let i = 0; i < s1.length; i++) {
+    result += s1[i] !== s2[i] ? "0" : s1[i];
+  }
+  return result;
+}
