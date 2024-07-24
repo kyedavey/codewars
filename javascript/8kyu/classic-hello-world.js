fcf1570ee0e95fbff0ccd04df9bdabfcd02b55a2
Kye Davey <kye.j.davey@gmail.com>
2024-07-24 11:38:56 +1000

    add new kata

---
+++ javascript/8kyu/classic-hello-world.js
@@ -0,0 +1,9 @@
+// You are given a method called main, make it print the line Hello World!, (yes, that includes a new line character at the end) and don't return anything
+// Note that for some languages, the function main is the entry point of the program.
+
+// My Solution
+class Solution {
+  static main() {
+    console.log("Hello World!");
+  }
+}
